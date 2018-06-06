d065fbec0e89d64ebb302f298b300e0256a5b2ff
Pierre Lorenzi <lorenzi.online@gmail.com>
2018-06-06 18:20:09 +0200

    To be more consistent, put Glyph in the Bitmap file

--- HyperCardCommon/HyperCardCommon/BitmapFont.swift
+++ HyperCardCommon/HyperCardCommon/BitmapFont.swift
@@ -61,3 +61,37 @@ public class BitmapFont {
     }
     
 }
+
+/// A glyph in a bitmap font.
+/// <p>
+/// It is a class, not a struct, so it can be lazily loaded and shared (the missing glyph in a font is
+/// always used for several characters).
+public class Glyph {
+    
+    /// The width of a character is the distance from the origin of the character to the origin of the next one.
+    public var width = 0
+    
+    ///  The offset is the position of the character bitmap relative to the character origin. Positive towards the right.
+    public var imageOffset = 0
+    
+    /// Top of image, from the baseline
+    public var imageTop = 0
+    
+    /// Width of the image
+    public var imageWidth: Int = 0
+    
+    /// Height of the image
+    public var imageHeight: Int = 0
+    
+    /// If there is an image
+    public var isThereImage: Bool = false
+    
+    /// The image of the character
+    public var image: MaskedImage? {
+        get { return imageProperty.value }
+        set { imageProperty.value = newValue }
+    }
+    public var imageProperty = Property<MaskedImage?>(nil)
+    
+}
+
