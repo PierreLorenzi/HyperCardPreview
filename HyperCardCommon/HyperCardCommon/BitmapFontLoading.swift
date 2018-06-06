b2692cb0bfce9b54726c793abca9aa4d514d0dbd
Pierre Lorenzi <lorenzi.online@gmail.com>
2018-06-06 16:59:05 +0200

    Rename the loading file to xxxLoading

--- HyperCardCommon/HyperCardCommon/BitmapFontLoading.swift
+++ HyperCardCommon/HyperCardCommon/BitmapFontLoading.swift
@@ -73,3 +73,40 @@ public extension BitmapFont {
     
 }
 
+/// Glyph with lazy loading from a file
+private extension Glyph {
+    
+    convenience init(maximumAscent: Int, maximumKerning: Int, fontRectangleHeight: Int, width: Int, offset: Int, startImageOffset: Int, endImageOffset: Int, loadBitImage: @escaping () -> Image) {
+        
+        self.init()
+        
+        self.width = width
+        self.imageOffset = maximumKerning + offset
+        self.imageTop = maximumAscent
+        self.imageWidth = endImageOffset - startImageOffset
+        self.imageHeight = fontRectangleHeight
+        self.isThereImage = (endImageOffset > startImageOffset)
+        self.imageProperty.lazyCompute { () -> MaskedImage? in
+            return Glyph.loadImage(startOffset: startImageOffset, endOffset: endImageOffset, loadBitImage: loadBitImage)
+        }
+    }
+    
+    private static func loadImage(startOffset: Int, endOffset: Int, loadBitImage: () -> Image) -> MaskedImage? {
+        
+        /* If the image has a null width, there is no image */
+        guard endOffset > startOffset else {
+            return nil
+        }
+        
+        /* Load the image */
+        let bitImage = loadBitImage()
+        let drawing = Drawing(width: endOffset - startOffset, height: bitImage.height)
+        drawing.drawImage(bitImage, position: Point(x: 0, y: 0), rectangleToDraw: Rectangle(top: 0, left: startOffset, bottom: bitImage.height, right: endOffset))
+        
+        return MaskedImage(image: drawing.image)
+        
+    }
+    
+}
+
+
