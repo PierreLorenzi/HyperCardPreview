7c1b407699bf020e24efd49731277c0caccf29dc
Pierre Lorenzi <lorenzi.online@gmail.com>
2018-09-28 10:41:57 +0200

    Handle sound resources

--- HyperCardPreview/ResourceController.swift
+++ HyperCardPreview/ResourceController.swift
@@ -61,9 +61,97 @@ class ResourceController: NSWindowController, NSCollectionViewDataSource {
                 let image = NSImage(data: pictureData)!
                 return ResourceContent.picture(image)
                 
+            case "snd ":
+                let possibleAiff = ResourceElement.convertSndToAIFF(data: self.data)
+                let possibleSound: NSSound?
+                if let aiff = possibleAiff {
+                    possibleSound = NSSound(data: aiff)
+                }
+                else {
+                    possibleSound = nil
+                }
+                return ResourceContent.sound(possibleSound)
+                
             default:
                 return ResourceContent.generic(data: self.data)
             }
+        }
+        
+        private static func convertSndToAIFF(data: DataRange) -> Data? {
+            
+            /* This is a very limited and messy function to make an AIFF
+             out of a snd resource. It is the fastest way I've found to play an old sound. */
+            
+            /* We only accept format 2 'snd ' resources, because HyperCard did (and it's easier that way) */
+            guard data.readUInt16(at: 0x0) == 2 else {
+                return nil
+            }
+            
+            /* The first command of a format 2 resource should be bufferCmd or soundCmd */
+            guard data.readUInt16(at: 0x6) == 0x8050 || data.readUInt16(at: 0x6) == 0x8051 else {
+                return nil
+            }
+            
+            /* Get the offset of the sampled sound */
+            let soundOffset = data.readUInt32(at: 0xA)
+            
+            /* Check that the sampled sound is in the data */
+            guard data.readUInt32(at: soundOffset + 0x0) == 0 else {
+                return nil
+            }
+            
+            /* Read the header of the sampled sound, which contain the parameters */
+            let byteCount = data.readUInt32(at: soundOffset + 0x4)
+            let sampleRateValue = data.readUInt32(at: soundOffset + 0x8)
+            let sampleRate: Double
+            switch sampleRateValue {
+            case 0xAC440000:
+                sampleRate = 44100.0
+            case 0x56EE8BA3:
+                sampleRate = 22254.54545
+            case 0x2B7745D1:
+                sampleRate = 11127.27273
+            default:
+                sampleRate = 11127.27273 //Double(sampleRateValue)
+            }
+            guard data.readUInt8(at: soundOffset + 0x14) == 0 else {
+                return nil
+            }
+            
+            /* Build the buffer */
+            let fileLength = 2*byteCount + 0x36
+            let aiffData = UnsafeMutableRawPointer.allocate(byteCount: fileLength, alignment: 4)
+            
+            /* Fill the AIFF fields in the data */
+            aiffData.advanced(by: 0x0).assumingMemoryBound(to: UInt32.self).pointee = UInt32(0x464F524D).byteSwapped
+            aiffData.advanced(by: 0x4).assumingMemoryBound(to: UInt32.self).pointee = UInt32(truncatingIfNeeded: fileLength - 4).byteSwapped
+            aiffData.advanced(by: 0x8).assumingMemoryBound(to: UInt32.self).pointee = UInt32(0x41494646).byteSwapped
+            aiffData.advanced(by: 0xC).assumingMemoryBound(to: UInt32.self).pointee = UInt32(0x434F4D4D).byteSwapped
+            aiffData.advanced(by: 0x10).assumingMemoryBound(to: UInt32.self).pointee = UInt32(18).byteSwapped
+            aiffData.advanced(by: 0x14).assumingMemoryBound(to: UInt16.self).pointee = UInt16(1).byteSwapped
+            aiffData.advanced(by: 0x16).assumingMemoryBound(to: UInt32.self).pointee = UInt32(truncatingIfNeeded: byteCount).byteSwapped
+            aiffData.advanced(by: 0x1A).assumingMemoryBound(to: UInt16.self).pointee = UInt16(16).byteSwapped
+            aiffData.advanced(by: 0x1C).assumingMemoryBound(to: Float80.self).pointee = Float80(sampleRate)
+            for i in 0..<5 {
+                let x = aiffData.advanced(by: 0x1C + i).assumingMemoryBound(to: UInt8.self).pointee
+                aiffData.advanced(by: 0x1C + i).assumingMemoryBound(to: UInt8.self).pointee = aiffData.advanced(by: 0x26 - 1 - i).assumingMemoryBound(to: UInt8.self).pointee
+                aiffData.advanced(by: 0x26 - 1 - i).assumingMemoryBound(to: UInt8.self).pointee = x
+            }
+            aiffData.advanced(by: 0x26).assumingMemoryBound(to: UInt32.self).pointee = UInt32(0x53534E44).byteSwapped
+            aiffData.advanced(by: 0x2A).assumingMemoryBound(to: UInt32.self).pointee = UInt32(truncatingIfNeeded: 2*byteCount + 8).byteSwapped
+            aiffData.advanced(by: 0x2E).assumingMemoryBound(to: UInt32.self).pointee = UInt32(0).byteSwapped
+            aiffData.advanced(by: 0x32).assumingMemoryBound(to: UInt32.self).pointee = UInt32(0).byteSwapped
+            
+            /* Fill the sound data (convert from 8-bit PCM to 16-bit PCM) */
+            for i in 0..<byteCount {
+                let byte = data.sharedData[data.offset + soundOffset + 0x16 + i]
+                let shiftedByte = byte &+ UInt8(128)
+                aiffData.advanced(by: 0x36 + 2*i).assumingMemoryBound(to: UInt8.self).pointee = shiftedByte
+            }
+            
+            /* Use the data */
+            let finalData = Data(bytesNoCopy: aiffData, count: fileLength, deallocator: Data.Deallocator.free)
+            return finalData
         }
     }
     
@@ -71,6 +159,7 @@ class ResourceController: NSWindowController, NSCollectionViewDataSource {
         case generic(data: DataRange)
         case icon(NSImage)
         case picture(NSImage)
+        case sound(NSSound?)
     }
     
     func setup(resourceFork possibleResourceFork: Data?) {
@@ -115,6 +204,8 @@ class ResourceController: NSWindowController, NSCollectionViewDataSource {
             item.imageView!.image = image
         case .picture(let image):
             item.imageView!.image = image
+        case .sound:
+            item.imageView!.image = NSImage(named: "SoundResourceIcon")
         }
         
         return item
