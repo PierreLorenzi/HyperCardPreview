b9992595b6a1e03073340fc01f54733eda766d02
Pierre Lorenzi <lorenzi.online@gmail.com>
2019-08-31 01:56:08 +0200

    Correct mistake

--- HyperCardCommon/HyperCardCommon/ExpressionSchemas.swift
+++ HyperCardCommon/HyperCardCommon/ExpressionSchemas.swift
@@ -14,107 +14,107 @@ public extension Schemas {
     
     
     
-    static let or = Schema<Expression>("\(Expression) or \(Expression)")
+    static let or = Schema<Expression>("\(expressionAgain) or \(expressionAgain)")
         
         .returns { Expression.operator(Operator.or($0, $1)) }
     
-    static let and = Schema<Expression>("\(Expression) and \(Expression)")
+    static let and = Schema<Expression>("\(expressionAgain) and \(expressionAgain)")
         
         .returns { Expression.operator(Operator.and($0, $1)) }
     
-    static let equal1 = Schema<Expression>("\(Expression) = \(Expression)")
+    static let equal1 = Schema<Expression>("\(expressionAgain) = \(expressionAgain)")
         
         .returns { Expression.operator(Operator.equal($0, $1)) }
     
-    static let equal2 = Schema<Expression>("\(Expression) is \(Expression)")
+    static let equal2 = Schema<Expression>("\(expressionAgain) is \(expressionAgain)")
         
         .returns { Expression.operator(Operator.equal($0, $1)) }
     
-    static let unequal1 = Schema<Expression>("\(Expression) ≠ \(Expression)")
+    static let unequal1 = Schema<Expression>("\(expressionAgain) ≠ \(expressionAgain)")
         
         .returns { Expression.operator(Operator.unequal($0, $1)) }
     
-    static let unequal2 = Schema<Expression>("\(Expression) <> \(Expression)")
+    static let unequal2 = Schema<Expression>("\(expressionAgain) <> \(expressionAgain)")
         
         .returns { Expression.operator(Operator.unequal($0, $1)) }
     
-    static let unequal3 = Schema<Expression>("\(Expression) is not \(Expression)")
+    static let unequal3 = Schema<Expression>("\(expressionAgain) is not \(expressionAgain)")
         
         .returns { Expression.operator(Operator.unequal($0, $1)) }
     
-    static let lesserThan = Schema<Expression>("\(Expression) < \(Expression)")
+    static let lesserThan = Schema<Expression>("\(expressionAgain) < \(expressionAgain)")
         
         .returns { Expression.operator(Operator.lesserThan($0, $1)) }
     
-    static let greaterThan = Schema<Expression>("\(Expression) > \(Expression)")
+    static let greaterThan = Schema<Expression>("\(expressionAgain) > \(expressionAgain)")
         
         .returns { Expression.operator(Operator.greaterThan($0, $1)) }
     
-    static let lesserThanOrEqual1 = Schema<Expression>("\(Expression) <= \(Expression)")
+    static let lesserThanOrEqual1 = Schema<Expression>("\(expressionAgain) <= \(expressionAgain)")
         
         .returns { Expression.operator(Operator.lesserThanOrEqual($0, $1)) }
     
-    static let lesserThanOrEqual2 = Schema<Expression>("\(Expression) ≤ \(Expression)")
+    static let lesserThanOrEqual2 = Schema<Expression>("\(expressionAgain) ≤ \(expressionAgain)")
         
         .returns { Expression.operator(Operator.lesserThanOrEqual($0, $1)) }
     
-    static let greaterThanOrEqual1 = Schema<Expression>("\(Expression) >= \(Expression)")
+    static let greaterThanOrEqual1 = Schema<Expression>("\(expressionAgain) >= \(expressionAgain)")
         
         .returns { Expression.operator(Operator.greaterThanOrEqual($0, $1)) }
     
-    static let greaterThanOrEqual2 = Schema<Expression>("\(Expression) ≥ \(Expression)")
+    static let greaterThanOrEqual2 = Schema<Expression>("\(expressionAgain) ≥ \(expressionAgain)")
         
         .returns { Expression.operator(Operator.greaterThanOrEqual($0, $1)) }
     
-    static let contains = Schema<Expression>("\(Expression) contains \(Expression)")
+    static let contains = Schema<Expression>("\(expressionAgain) contains \(expressionAgain)")
         
         .returns { Expression.operator(Operator.contains($0, $1)) }
     
-    static let isIn = Schema<Expression>("\(Expression) is in \(Expression)")
+    static let isIn = Schema<Expression>("\(expressionAgain) is in \(expressionAgain)")
         
         .returns { Expression.operator(Operator.isIn($0, $1)) }
     
-    static let isNotIn = Schema<Expression>("\(Expression) is not in \(Expression)")
+    static let isNotIn = Schema<Expression>("\(expressionAgain) is not in \(expressionAgain)")
         
         .returns { Expression.operator(Operator.isNotIn($0, $1)) }
     
-    static let isOfType = Schema<Expression>("\(Expression) is \(either: "a", "an") \(expressionType)")
+    static let isOfType = Schema<Expression>("\(expressionAgain) is \(either: "a", "an") \(expressionType)")
         
         .returns { Expression.operator(Operator.isOfType($0, $1)) }
     
-    static let concatenation = Schema<Expression>("\(Expression) & \(Expression)")
+    static let concatenation = Schema<Expression>("\(expressionAgain) & \(expressionAgain)")
         
         .returns { Expression.operator(Operator.concatenation($0, $1)) }
     
-    static let concatenationWithSpace = Schema<Expression>("\(Expression) && \(Expression)")
+    static let concatenationWithSpace = Schema<Expression>("\(expressionAgain) && \(expressionAgain)")
         
         .returns { Expression.operator(Operator.concatenationWithSpace($0, $1)) }
     
-    static let addition = Schema<Expression>("\(Expression) + \(Expression)")
+    static let addition = Schema<Expression>("\(expressionAgain) + \(expressionAgain)")
         
         .returns { Expression.operator(Operator.addition($0, $1)) }
     
-    static let substraction = Schema<Expression>("\(Expression) - \(Expression)")
+    static let substraction = Schema<Expression>("\(expressionAgain) - \(expressionAgain)")
         
         .returns { Expression.operator(Operator.substraction($0,$1)) }
     
-    static let multiplication = Schema<Expression>("\(Expression) * \(Expression)")
+    static let multiplication = Schema<Expression>("\(expressionAgain) * \(expressionAgain)")
         
         .returns { Expression.operator(Operator.multiplication($0,$1)) }
     
-    static let division = Schema<Expression>("\(Expression) / \(Expression)")
+    static let division = Schema<Expression>("\(expressionAgain) / \(expressionAgain)")
         
         .returns { Expression.operator(Operator.division($0,$1)) }
     
-    static let modulo = Schema<Expression>("\(Expression) mod \(Expression)")
+    static let modulo = Schema<Expression>("\(expressionAgain) mod \(expressionAgain)")
         
         .returns { Expression.operator(Operator.modulo($0,$1)) }
     
-    static let integerDivision = Schema<Expression>("\(Expression) div \(Expression)")
+    static let integerDivision = Schema<Expression>("\(expressionAgain) div \(expressionAgain)")
         
         .returns { Expression.operator(Operator.integerDivision($0,$1)) }
 
-    static let exponentiation = Schema<Expression>("\(Expression) ^ \(Expression)")
+    static let exponentiation = Schema<Expression>("\(expressionAgain) ^ \(expressionAgain)")
         
         .returns { Expression.operator(Operator.exponentiation($0,$1)) }
 
