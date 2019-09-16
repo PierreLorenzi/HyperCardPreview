bbe9167899f444e5a57cfd7aa576c0e82d0f2fe4
Pierre Lorenzi <lorenzi.online@gmail.com>
2019-09-16 12:52:02 +0200

    Cite ffmpeg for the MACE compression

--- HyperCardPreview/SoundCompression.swift
+++ HyperCardPreview/SoundCompression.swift
@@ -6,6 +6,34 @@
 //  Copyright © 2019 Pierre Lorenzi. All rights reserved.
 //
 
+/* Algorithm taken in the 'mace.c' file of the FFmpeg library
+ Here are the comments printed in the original file: */
+/*
+ * MACE decoder
+ * Copyright (c) 2002 Laszlo Torok <torokl@alpha.dfmk.hu>
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+/*
+ * Adapted to libavcodec by Francois Revol <revol@free.fr>
+ * (removed 68k REG stuff, changed types, added some statics and consts,
+ * libavcodec api, context stuff, interlaced stereo out).
+ */
+
 import HyperCardCommon
 
 
